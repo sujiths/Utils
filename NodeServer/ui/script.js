fc2f809aab06dd3ce7a4de58c24b8e1fdc73b11f
Sujith S <digitizedx@gmail.com>
2021-11-03 18:41:07 +0530

    clean up

--- NodeServer/ui/script.js
+++ NodeServer/ui/script.js
@@ -1,11 +1,9 @@
-async function do_upload() {
+function do_upload() {
     let data = document.getElementById("file").files[0];
     const formData = new FormData();
     formData.append('minidump', data);
     var spin = document.getElementById("spinner");
     spin.classList.add("spinner-border");
-
-    spinner.in
     console.log('do_upload ', data)
     fetch("http://localhost:80/dump", {
         method: 'POST',
