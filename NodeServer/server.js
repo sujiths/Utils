e8e51ee61738bf8f37d3b24da8495b783b3fc333
Sujith S <sujithsudhakaran@gmail.com>
2021-11-03 18:26:20 +0530

    Working node server

---
+++ NodeServer/server.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var app = express();
+var multer = require('multer');
+var upload = multer();
+const fs = require('fs')
+const fspromises = require('fs/promises')
+
+
+/* Adding a middleware for preflight */
+app.use(function (req, res, next) {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,boundary');
+   next();
+});
+
+app.use(upload.single('minidump'));
+app.use(express.static('ui'));
+
+app.get("/", function (req, res) {
+   res.sendFile('ui/index.html', { root: '.' });
+})
+
+app.post('/dump', function (req, res) {
+   console.log(req.file);
+   const ret = write_dump_file(req.file.originalname, req.file.buffer);
+   ret.then(success => {
+      return execute_shell(req.file.originalname);
+   }, fail => { console.error("Failed to write the file!!!!"); }).then(
+      success => {
+         callback(res, req.file.originalname);
+      }, fail => {
+         console.error("Failed to execute command!!!");
+      }
+   );
+})
+
+
+async function write_dump_file(filename, data) {
+   const pr = await fspromises.writeFile(filename, data);
+   return pr;
+}
+
+function execute_shell(filename) {
+   return new Promise((resolve, reject) => {
+
+      const { spawn } = require('child_process');
+      const ls = spawn('cat', [filename]);
+
+      ls.stdout.on('data', (data) => {
+         console.log(`stdout: ${data}`);
+      });
+
+      ls.stderr.on('data', (data) => {
+         console.log(`stderr: ${data}`);
+      });
+
+      ls.on('close', (code) => {
+         if (code == 0) {
+            console.error("Command succeded!!!");
+            resolve();
+         }
+         else {
+            console.error("Command execution failed!!!");
+            reject();
+         }
+      });
+   });
+}
+
+function callback(res, file) {
+   res.json({ filename: file });
+}
+
+var server = app.listen(80, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Server is listening at http://%s:%s", host, port)
+})
---
+++ NodeServer/ui/script.js
@@ -0,0 +1,15 @@
+async function do_upload() {
+    let data = document.getElementById("file").files[0];
+    const formData = new FormData();
+    formData.append('minidump', data);
+    var spin = document.getElementById("spinner");
+    spin.classList.add("spinner-border");
+
+    spinner.in
+    console.log('do_upload ', data)
+    fetch("http://localhost:80/dump", {
+        method: 'POST',
+        body: formData
+    }).then(response => response.json())
+        .then(val => { alert(val.filename); console.log(val.filename); spin.classList.remove("spinner-border")});
+};
